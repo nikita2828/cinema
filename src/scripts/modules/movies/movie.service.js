e74d249d69ca746d554b1638ee6f612b60d0fa9e
Belenov Dmitriy <dimabelenov@gmail.com>
2020-10-03 21:17:56 +0300

    adds erros in create movie form

--- src/scripts/modules/movies/movie.service.js
+++ src/scripts/modules/movies/movie.service.js
@@ -6,16 +6,17 @@ let film_create_wrapper = document.querySelector('.film-create-wrapper');
 let body = document.querySelector('body');
 let btn_close = document.querySelector('.film-create-btn-close');
 
-let name = document.querySelector('.film-create-name');
-let year = document.querySelector('.film-create-year');
-let director = document.querySelector('.film-create-director');
-let role = document.querySelector('.film-create-role');
-let img = document.querySelector('.film-create-img');
-let video = document.querySelector('.film-create-video');
-let genre = document.querySelectorAll('.film-genre-input');
-let description = document.querySelector('.film-create-description');
-let btn_add = document.querySelector('.film-create-btn-add');
-let film_wrapper = document.querySelector('.film');
+let nameNode = document.querySelector('.film-create-name');
+let yearNode = document.querySelector('.film-create-year');
+let directorNode = document.querySelector('.film-create-director');
+let roleNode = document.querySelector('.film-create-role');
+let imgNode = document.querySelector('.film-create-img');
+let videoNode = document.querySelector('.film-create-video');
+let genreNode = document.querySelectorAll('.film-genre-input');
+let descriptionNode = document.querySelector('.film-create-description');
+let btnAddNode = document.querySelector('.film-create-btn-add');
+let filmWrapperNode = document.querySelector('.film');
+
 //film
 function getFilm() {
 	film_wrapper.innerHTML = '';
@@ -60,62 +61,6 @@ window.addEventListener('click', (event) => {
 	if (event.target == film_create_wrapper) {
 		film_create_wrapper.style.display = 'none';
 	}
-});
-
-// add film
-
-function add_new_film() {
-	btn_add.addEventListener('click', () => {
-		let film_add = {
-			name: name.value,
-			year: +year.value,
-			director: director.value,
-			role: role.value,
-			img: img.value,
-			video: video.value,
-			genre: [...genre]
-				.map((g) => {
-					if (g.checked) {
-						return g.value;
-					}
-				})
-				.filter(Boolean),
-			description: description.value,
-		};
-		film.addMovie(film_add);
-		getFilm();
-		close_handler();
-	});
-}
-
-//validation
-const check_inputs = [name, year, director, role, img, video, description];
-
-check_inputs.forEach((input) => {
-	input.addEventListener('blur', () => {
-		if (!input.value) {
-			input.style.border = '2px solid rgb(218,18,26)';
-		}
-	});
-	input.addEventListener('blur', () => {
-		if (input.value) {
-			input.style.border = '2px solid rgb(6,144,207)';
-		}
-	});
-});
-
-btn_add.addEventListener('click', () => {
-	check_inputs.forEach((input) => {
-		if (!input.value) {
-			input.style.border = '2px solid rgb(218,18,26)';
-		} else if (input.value) {
-			input.style.border = '2px solid rgb(6,144,207)';
-			add_new_film();
-			input.value = '';
-		} else {
-			alert('Everything is bad');
-		}
-	});
 });
 
 //search
