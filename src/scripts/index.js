43fdb5ed5b8f45c8dd43556c63cde547111191d5
Belenov Dmitriy <dimabelenov@gmail.com>
2020-10-03 21:03:02 +0300

    changed project structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
-import "../styles/index.scss";
-import "./modules/data_film.js";
-import "./modules/film_service.js";
-if (process.env.NODE_ENV === "development") {
-  require("../index.html");
+import '../styles/index.scss';
+import './modules/movies/data';
+import './modules/movies/movie.service';
+if (process.env.NODE_ENV === 'development') {
+	require('../index.html');
 }
---
+++ src/scripts/modules/movies/movie.template.js
@@ -0,0 +1,20 @@
+export const film_one = `
+<div class="film-name">
+<span class="name"></span>
+<div class="film-btn">
+  <button class="change-btn btn btn-success">Изменить</button>
+  <button class="delete-btn btn btn-danger">Удалить</button>
+ </div>
+</div>
+<div class="film-card">
+  <img class="img" width="145px" height="200px" src="" alt="" />
+  
+    <p class="year"></p>
+    <p class="country"></p>
+    <p class="genre"></p>
+    <p class="director"><p>
+    <p class="role"></p>
+    <p class="description"></p>
+  
+</div>
+`;
